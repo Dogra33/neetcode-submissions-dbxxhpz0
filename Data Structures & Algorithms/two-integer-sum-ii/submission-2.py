0d84939137123c49046aa47892064aa1b4604275
Dogra33 <dogra156@gmail.com>
2026-04-03 21:30:52 +0530

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        mapper = {}
+        for i in range(0,len(numbers)):
+            if numbers[i] not in mapper:
+                mapper[numbers[i]] = i+1
+        print(mapper)
+
+        for num in numbers:
+            res = []
+            find = target - num 
+            print(find)
+            if find in mapper:
+                res.append(mapper[num])
+                res.append(mapper[find])
+                return res
+        return []
